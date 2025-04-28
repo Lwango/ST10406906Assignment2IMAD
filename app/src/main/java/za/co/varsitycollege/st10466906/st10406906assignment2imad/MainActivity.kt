19b93972fbe7deed7b506aaba35736190fa49bc4
Lwango <ST10466906@vcconnect.edu.za>
2025-04-28 16:34:42 +0200

    Created second MainActivity for quizz questions.

---
+++ app/src/main/java/za/co/varsitycollege/st10466906/st10406906assignment2imad/MainActivity.kt
@@ -0,0 +1,30 @@
+package za.co.varsitycollege.st10466906.st10406906assignment2imad
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val begin: Button = findViewById<Button>(R.id.begin)
+        begin.setOnClickListener { val intent = Intent(this,QuizzquestionsActivity::class.java)
+        startActivity(intent)
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/za/co/varsitycollege/st10466906/st10406906assignment2imad/QuizzquestionsActivity.kt
@@ -0,0 +1,20 @@
+package za.co.varsitycollege.st10466906.st10406906assignment2imad
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuizzquestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.quizzquestions)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
